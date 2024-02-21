ce464a1f54f5d6574107ef369080a648dde0584f
Sagarrafi26 <138652492+Sagarrafi26@users.noreply.github.com>
2024-02-21 14:47:22 +0530

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,9 @@
-import Header from '../components/Header';
-import Table from '../components/Table';
-import Dropdown from '../components/Dropdown'
-import Dropdown2 from '../components/Dropdown2'
+import Header from '../Components/Header';
+import Table from '../Components/Table';
+import Dropdown from '../Components/Dropdown'
+import Dropdown2 from '../Components/Dropdown2'
 import Constants from '../constants/Constants';
+import { Link } from 'react-router-dom';
 
 function Home () {
 
@@ -16,7 +17,7 @@ function Home () {
     <div className="Home">
       <Header pageTitle={Constants.COMPANY_MASTER}/>
 
-      <div className='flex w-[100%]'>
+      <div className='flex w-[100%] py-2'>
         <div className=' flex  '>
           <p className='w-[100%]'> {Constants.COMPANY_NAME} </p> 
 
@@ -26,21 +27,21 @@ function Home () {
         </div>
 
         <div className=' flex '>
-          <p className='mx-10'> {Constants.ACTIVE_STATUS} </p>
+          <p className=''> {Constants.ACTIVE_STATUS} </p>
           <div className='  w-[100%] rounded-1-md px-5 py-0 flex' >
             <Dropdown2/>
           </div>
         </div>
           
 
-        <div className='flex  w-1/6  justify-center  cursor-pointer main-w-[200%] relative mx-20  rounded-md bg-gray-700 '>
+        <div className='flex  w-1/6  justify-center  cursor-pointer main-w-[200%] relative mx-20  rounded-md bg-gray-400 '>
           <button>
             {Constants.SEARCH} 
           </button>
         </div>
 
 
-        <div className='flex w-1/6  justify-center  cursor-pointer main-w-[200%] relative  rounded-md bg-gray-700 '>
+        <div className='flex w-1/6  justify-center  cursor-pointer main-w-[200%] relative  rounded-md bg-gray-400 '>
           <button>
             {Constants.CLEAR} 
           </button>
@@ -51,16 +52,19 @@ function Home () {
         <hr></hr>
       </div>
 
-      <div className='flex text-end justify-end  py-3'>
-        <div className='flex  w-1/6  justify-center  cursor-pointer main-w-[200%] relative mx-20 py-3 me-10  rounded-md bg-gray-700 '>
-          <button >
-            {Constants.ADD}
-          </button>
+      <div className='flex text-end justify-end   py-3'>
+        <div className='flex  w-1/6  justify-center  cursor-pointer main-w-[200%] relative mx-20 py-3 me-10  rounded-md bg-gray-400 '>
+          <Link to='/company/add'>
+            <button>{Constants.ADD}</button>
+            </Link>
+         
         </div>
-        <div className='flex w-1/6  justify-center  cursor-pointer main-w-[200%] relative ms-2  rounded-md bg-gray-700 '>
+        <div className='flex w-1/6  justify-center text-right  cursor-pointer main-w-[200%] relative ms-2 py-2  rounded-md bg-gray-400 '>
+          <Link to="/company/edit">
           <button>
             {Constants.EDIT}
           </button>
+          </Link>
         </div> 
       </div>
 
