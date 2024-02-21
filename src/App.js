ea4463a434c06215ad1c92503d0c75593385b312
Sagarrafi26 <138652492+Sagarrafi26@users.noreply.github.com>
2024-02-21 14:50:10 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
+import { Addpage } from './pages/Addpage';
+import { EditPage } from './pages/EditPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={process.env.PUBLIC_URL + "/"} element={<Home />} />
+        <Route path="/company/add" Component={Addpage}/>
+        <Route path="/company/edit" Component={EditPage}/>
       </Routes>
     </BrowserRouter>
   );
