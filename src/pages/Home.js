d911f0afa046cfc558d7baedc730aa2df72e10b3
Sagarrafi26 <138652492+Sagarrafi26@users.noreply.github.com>
2024-02-22 12:57:57 +0530

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,11 @@
-import Header from '../components/Header';
-import Table from '../components/Table';
-import Dropdown from '../components/Dropdown'
-import Dropdown2 from '../components/Dropdown2'
+import Header from '../Components/Header';
+import Table from '../Components/Table';
+import Dropdown from '../Components/Dropdown'
+import Dropdown2 from '../Components/Dropdown2'
 import Constants from '../constants/Constants';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Home () {
-
-  const navigate = useNavigate();
 
   const data = [
     { id: 1, companyName: 'Company A', companyCode: 'A001', active: true },
@@ -15,45 +13,36 @@ function Home () {
     { id: 3, companyName: 'Company C', companyCode: 'C003', active: true },
   ];
 
-  const handleAddBtn = () => {
-    navigate("/company/add");
-  };
-
-  const handleEditBtn = () => {
-    navigate("/company/edit");
-  };
-
   return (
-    <div>
+    <div className="Home">
       <Header pageTitle={Constants.COMPANY_MASTER}/>
 
-      <div className='flex justify-center py-2'>
-        <div className=' flex mx-5'>
-          <p className='w-[100%]'> {Constants.COMPANY_NAME} </p> 
-          <div className='ml-2 rounded-1-md flex' >
+      <div className='flex w-[100%] py-2'>
+        <div className=' flex justify-center '>
+          <p className='w-[100%] py-2'> {Constants.COMPANY_NAME} </p> 
+
+          <div className=' mx-6 rounded-1-md px-5 py-0 flex me-7 ' >
             <Dropdown />
           </div>
         </div>
 
-        <div className=' flex mx-5'>
-          <p className='w-[100%]'> {Constants.ACTIVE_STATUS} </p>
-          <div className='ml-2 rounded-1-md flex' >
+        <div className=' flex '>
+          <p className='w-[100%] py-2'> {Constants.ACTIVE_STATUS} </p>
+          <div className='  w-[100%] rounded-1-md px-5 py-0  justify-end' >
             <Dropdown2/>
           </div>
         </div>
           
 
-        <div className='flex justify-center mb-5 mx-5 '>
-          <button
-            className='flex justify-center w-full sm:w-60 font-medium rounded-lg text-sm px-5 py-2 text-center border border-2 text-white bg-gradient-to-t from-gray-500 to-gray-600 hover:bg-gradient-to-b'>
+        <div className='flex  w-[220px]  justify-center  cursor-pointer  mx-20 py-3  rounded-md bg-gray-400 '>
+          <button>
             {Constants.SEARCH} 
           </button>
         </div>
 
 
-        <div className='flex justify-center mb-5 mx-5'>
-          <button
-            className='flex justify-center w-full sm:w-60 font-medium rounded-lg text-sm px-5 py-2 text-center border border-2 text-white bg-gradient-to-t from-gray-500 to-gray-600 hover:bg-gradient-to-b'>
+        <div className='flex w-[220px]  justify-center  cursor-pointer    rounded-md bg-gray-400 '>
+          <button>
             {Constants.CLEAR} 
           </button>
         </div>
@@ -63,23 +52,19 @@ function Home () {
         <hr></hr>
       </div>
 
-      <div className='flex text-end justify-end px-5 py-3'>
-
-        <div className='flex justify-center mb-5 mx-2'>
-            <button 
-              className='flex justify-center w-full sm:w-40 font-medium rounded-lg text-sm px-5 py-2 text-center border border-2 text-white bg-gradient-to-t from-gray-500 to-gray-600 hover:bg-gradient-to-b'
-              onClick={handleAddBtn}>
-              {Constants.ADD}
-            </button>
-        
+      <div className='flex text-end justify-end  py-3'>
+        <div className='flex  w-[220px]  justify-center  cursor-pointer  mx-20 py-3   rounded-md bg-gray-400 '>
+          <Link to='/company/add'>
+            <button>{Constants.ADD}</button>
+            </Link>
+         
         </div>
-        
-        <div className='flex justify-center mb-5 mx-2'>
-          <button 
-            className='flex justify-center w-full sm:w-40 font-medium rounded-lg text-sm px-5 py-2 text-center border border-2 text-white bg-gradient-to-t from-gray-500 to-gray-600 hover:bg-gradient-to-b' 
-            onClick={handleEditBtn}>
+        <div className='flex w-[220px]  justify-center   cursor-pointer   mx-[2px] py-2  rounded-md bg-gray-400 '>
+          <Link to="/company/edit">
+          <button>
             {Constants.EDIT}
           </button>
+          </Link>
         </div> 
       </div>
 
