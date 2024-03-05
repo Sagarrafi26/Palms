056cdf5caff0ec4a28b3ba0f0cb30f0c1d28b3bd
Soumya Raj <soumya.raj@technoforte.co.in>
2024-03-05 12:03:37 +0530

    edited the db connection implementation

--- Backend/server.js
+++ Backend/server.js
@@ -12,20 +12,39 @@ const config = {
   password: 'techno-123',
 };
 
+// Create a connection pool
+const poolPromise = new sql.ConnectionPool(config)
+  .connect()
+  .then(pool => {
+    console.log('Connected to SQL Server');
+    return pool;
+  })
+  .catch(err => console.log('Database Connection Failed! Bad Config: ', err));
+
 // API endpoint to fetch table data
+// app.get('/data', async (req, res) => {
+//   try {
+//       await sql.connect(config);
+//       const result = await sql.query('SELECT * FROM City');
+//       res.json(result.recordset);
+//   } catch (error) {
+//       console.error('Error fetching data:', error);
+//       res.status(500).json({ error: 'Error fetching data' });
+//   } finally {
+//       sql.close();
+//   }
+// });
+
 app.get('/data', async (req, res) => {
   try {
-      await sql.connect(config);
-      const result = await sql.query('SELECT * FROM City');
-      res.json(result.recordset);
+    const pool = await poolPromise;
+    const result = await pool.request().query('SELECT * FROM City');
+    res.json(result.recordset);
   } catch (error) {
-      console.error('Error fetching data:', error);
-      res.status(500).json({ error: 'Error fetching data' });
-  } finally {
-      sql.close();
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Error fetching data' });
   }
 });
- 
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
