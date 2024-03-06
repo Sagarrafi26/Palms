e0b06634084b0d63e1e639d018d40487abec9cd7
Sagarrafi26 <138652492+Sagarrafi26@users.noreply.github.com>
2024-03-06 10:57:43 +0530

    Add files via upload

--- Backend/server.js
+++ Backend/server.js
@@ -1,45 +1,33 @@
 const express = require('express');
-const sql = require('mssql');
- 
+const sql = require('mssql/msnodesqlv8');
+const cors = require('cors');
+
 const app = express();
+app.use(cors());
 const port = 8081;
- 
 
-const config = {
-  server: 'TECHNO-511\\SQLDEV2019',
-  database: 'PALMS-9.1',
-  user: 'sa',
-  password: 'techno-123',
-};
+app.get('/test', async (req, res) => {
+  // config for your database
+  const config = {
+    server: 'TECHNO-511\\SQLDEV2019',
+    database: 'PALMS-9.1',
+    user: 'sa',
+    password: 'techno-123',
+    driver: 'msnodesqlv8',
+    options: {
+      trustedConnection: true
+    }
+  };
 
-// Create a connection pool
-const poolPromise = new sql.ConnectionPool(config)
-  .connect()
-  .then(pool => {
-    console.log('Connected to SQL Server');
-    return pool;
-  })
-  .catch(err => console.log('Database Connection Failed! Bad Config: ', err));
-
-// API endpoint to fetch table data
-// app.get('/data', async (req, res) => {
-//   try {
-//       await sql.connect(config);
-//       const result = await sql.query('SELECT * FROM City');
-//       res.json(result.recordset);
-//   } catch (error) {
-//       console.error('Error fetching data:', error);
-//       res.status(500).json({ error: 'Error fetching data' });
-//   } finally {
-//       sql.close();
-//   }
-// });
-
-app.get('/data', async (req, res) => {
   try {
-    const pool = await poolPromise;
-    const result = await pool.request().query('SELECT * FROM City');
-    res.json(result.recordset);
+    console.log('starting sql');
+    const pool = await new sql.ConnectionPool(config).connect();
+    const result = await pool.request().query('select * from City');
+    console.log('ending sql');
+    res.json({
+      data: result.recordset
+    });
+    pool.close();
   } catch (error) {
     console.error('Error fetching data:', error);
     res.status(500).json({ error: 'Error fetching data' });
